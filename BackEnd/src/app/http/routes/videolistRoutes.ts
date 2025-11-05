a30035e9a005c8ed25b39507844def891b62142b
jonimax11000 <jonimax11000@gmail.com>
2025-11-05 12:13:07 +0100

    backend con arquitectura clean

--- BackEnd/src/app/http/routes/videolistRoutes.ts
+++ BackEnd/src/app/http/routes/videolistRoutes.ts
@@ -1,16 +1,31 @@
-import express from "express";
-import { getVideos,getVideosPerId,getVideosPerThumbnail} from "../controllers/videoController";
-import { get } from "http";
+import { Router } from "express";
+import { VideoRepositoryIntern } from "../../infraestructure/datasorces/intern/VideoRepositoryIntern"
+import { GetVideosUseCase } from "../../domain/usecases/vdeo/GetVideosUseCase";
+import { GetVideoByIdUseCase } from "../../domain/usecases/vdeo/GetUserByIdUseCase";
+import { GetVideoByThumbnailUseCase } from "../../domain/usecases/vdeo/GetUserByThmbnailUseCase";
+import { VideoController } from "../controllers/videoController";
 
-const userRouter = express.Router();
+
+const repo = new VideoRepositoryIntern();
+
+// Creem el Controlador per als usuaris, proporcionant-li
+// instàncies dels casos d'ús, que al seu temps hem inicialitzat
+// amb el repositori (injecció de dependències)
+const controller = new VideoController(
+  new GetVideosUseCase(repo),
+  new GetVideoByIdUseCase(repo),
+  new GetVideoByThumbnailUseCase(repo)
+);
+
+const videolistRoutes = Router();
 
 
-userRouter.get("/", getVideos); 
-userRouter.get("/:id", getVideosPerId);
-userRouter.get("/:topic", getVideosPerThumbnail);
+videolistRoutes.get("/", controller.getAll); 
+videolistRoutes.get("/:id", controller.getById);
+videolistRoutes.get("/byTopic/:topic", controller.getByThumbnail);
 
 
 // Protegim la ruta per 
 //userRouter.post("/create", authMiddleware, createUser);
 
-export default userRouter;
+export default videolistRoutes;
